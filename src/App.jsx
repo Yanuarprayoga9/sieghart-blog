57fbe2e0d91653d91464f58cddcfd1a48d9d2a58
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-01 12:07:57 +0700

    feat: 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import NotFound from './pages/404'
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<NotFound />} />
         
       </Routes>
         
---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div
+            classname="min-h-screen flex justify-content-center items-center"
+        >NotFound</div>
+    )
+}
+
+export default NotFound
