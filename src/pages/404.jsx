e91761f717448d1734b2c7de163982572124c40a
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-01 12:27:03 +0700

    fix: reinstal

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div
-            classname="min-h-screen flex justify-content-center items-center"
-        >NotFound</div>
+            classname="min-h-screen flex justify-content-center items-center text-2xl flex-col"
+        >NotFound
+            <Link to={'/'} classname="text-slate-700">Go home</Link>
+        </div>
     )
 }
 
